f23d48c089d7a3054d7b19410c4cf76d0ddd5b29
amc96@cam.ac.uk <amc96@cam.ac.uk@efa14470-2078-11de-9d52-39b120432c5d>
2008-12-18 23:17:21 +0000

    Added Newitems_Notify to the local state
    git-svn-id: https://dtella-cambridge.googlecode.com/svn/trunk@13 efa14470-2078-11de-9d52-39b120432c5d

--- dtella-cambridge-1.2.3/dtella/common/state.py
+++ dtella-cambridge-1.2.3/dtella/common/state.py
@@ -280,6 +280,22 @@ class Persistent(LoadSaver):
         self.packValue(d, 'B', bool(state.persistent))
 
 
+#Added 18/12/2008 by andyhhp - support for newstuff functionality
+class Newitems_Notify(LoadSaver):
+	
+	key = 'newitems_notify'
+	
+	def load(self, state, d):
+		try:
+			state.newitems_notify = bool(self.unpackValue(d, 'B'))
+		except StateError:
+			state.newitems_notify = False
+	
+	
+	def save(self, state, d):
+		self.packValue(d, 'B', bool(state.newitems_notify))
+
+
 
 class LocalSearch(LoadSaver):
 
@@ -411,6 +427,7 @@ class DNSPkHashes(LoadSaver):
 
 
 client_loadsavers = [Persistent(),
+					 Newitems_Notify(),
                      LocalSearch(),
                      UDPPort(),
                      IPCache(),
