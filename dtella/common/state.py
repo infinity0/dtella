b96a114b8b9efc12703678e8e27fedef8190b233
infinity0x <infinity0x@efa14470-2078-11de-9d52-39b120432c5d>
2009-08-25 14:37:06 +0000

    - whoops, reinsert import
    git-svn-id: https://dtella-cambridge.googlecode.com/svn/branches/adc@280 efa14470-2078-11de-9d52-39b120432c5d

--- dtella/common/state.py
+++ dtella/common/state.py
@@ -21,7 +21,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
-import shelve, random, time, socket, struct
+import shelve, random, time, socket, struct, heapq
 
 from dtella.common.util import dcall_discard, get_user_path, CHECK
 from dtella.common.ipv4 import Ad
