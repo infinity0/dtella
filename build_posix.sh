59f8ef70e1592ef347471c92e6ced737019482d4
infinity0x <infinity0x@efa14470-2078-11de-9d52-39b120432c5d>
2009-12-06 19:08:18 +0000

    don't override already-made sdist when running stdeb
    git-svn-id: https://dtella-cambridge.googlecode.com/svn/branches/adc@350 efa14470-2078-11de-9d52-39b120432c5d

--- build_posix.sh
+++ build_posix.sh
@@ -5,16 +5,12 @@ eval $(python makevars.py)
 BLDDIR="build"
 OUTDIR="dist"
 
-# builds a source distribution of dtella
-python setup.py sdist --formats=bztar
-
-# builds a shell script that installs dtella and its dependencies
-# from a remote source distribution
-python setup.py bdist_shinst "--SVNR=http://dtella-cambridge.googlecode.com/svn/trunk" \
-	"--EXT=tar.bz2" "--EXT-CMD=tar xjf" "--EXT-VRB=tar xvjf" "--EXT-LST=tar tjf" \
-	"--DEPS=dtella_deps.tar.bz2"
-
-if [ "$1" = "deb" ]; then
+case $1 in
+deb )
+	if [ ! -f "dist/$FILEBASE.tar.gz" ]; then
+		echo "Need to build source distribution first."
+		exit 1
+	fi
 
 	# builds a debian package of dtella in deb_dist/
 	# you need stdeb 0.4.1 or greater
@@ -24,9 +20,10 @@ if [ "$1" = "deb" ]; then
 	# if the version string has a non-alphanum character in it, the build will fail.
 	# see http://github.com/astraw/stdeb/issues/#issue/15 for a workaround
 
-	py2dsc --workaround-548392=false -d "$BLDDIR/stdeb" -x "setup.cfg" "dist/$FILEBASE.tar.bz2"
+	py2dsc --workaround-548392=false -d "$BLDDIR/stdeb" -x "setup.cfg" "dist/$FILEBASE.tar.gz"
 	# bug #548392 does not affect us and the workaround restricts the python version, so don't use it
 
+	exit
 	cd "$BLDDIR/stdeb/$FILEBASE"
 	dpkg-buildpackage -rfakeroot
 	cd -
@@ -34,4 +31,16 @@ if [ "$1" = "deb" ]; then
 		if [ -f "$i" ]; then cp -a "$i" "$OUTDIR"; fi
 	done
 
-fi
+	;;
+* )
+	# builds a source distribution of dtella
+	python setup.py sdist
+	python setup.py sdist --formats=bztar
+
+	# builds a shell script that installs dtella and its dependencies
+	# from a remote source distribution
+	python setup.py bdist_shinst "--SVNR=http://dtella-cambridge.googlecode.com/svn/trunk" \
+		"--EXT=tar.bz2" "--EXT-CMD=tar xjf" "--EXT-VRB=tar xvjf" "--EXT-LST=tar tjf" \
+		"--DEPS=dtella_deps.tar.bz2"
+	;;
+esac
